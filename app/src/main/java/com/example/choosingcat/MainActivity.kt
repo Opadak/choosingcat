c483d4a6ca24addd28e9adbd287345bf3283a28e
Opadak <tfk1019@naver.com>
2021-02-03 21:45:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/choosingcat/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.choosingcat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        first_xml_btn_start.setOnClickListener {
+            setContentView(R.layout.activity_main)
+
+
+
+        }
+    }
+}
