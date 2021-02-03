14836c4f5f147801f1ac1c696bc6d27445070eed
Opadak <tfk1019@naver.com>
2021-02-04 02:42:14 +0900

    2021-02-04 (멀티 터치 넘 어려벙)

--- app/src/main/java/com/example/choosingcat/MainRandomActivity.kt
+++ app/src/main/java/com/example/choosingcat/MainRandomActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.MotionEvent
 import androidx.core.view.MotionEventCompat.getPointerId
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.random_main.*
+import kotlin.random.Random
 
 class MainRandomActivity : AppCompatActivity() {
 
@@ -27,20 +29,34 @@ class MainRandomActivity : AppCompatActivity() {
         if (event != null) {
             for (i in 0..pointCount) {
                 mActivePointerId = event.getPointerId(i)
+                val (x: Float, y: Float) = event.findPointerIndex(mActivePointerId).let { pointerIndex ->
+                    // Get the pointer's current position
+                    event.getX(pointerIndex) to event.getY(pointerIndex)
+
+                } // id 마다 위치를 가지고 오는거. 굳이 필요한가..?
 
             }
         }else false
         // 다중 터치 id 부여
 
-        val (x: Float, y: Float) = event.findPointerIndex(mActivePointerId).let { pointerIndex ->
-            // Get the pointer's current position
-            event.getX(pointerIndex) to event.getY(pointerIndex)
-        } //
+           val result_random =  rand(1,pointCount)
 
+
+
+
+            test.setText("$result_random")
 
 
         return super.onTouchEvent(event)
 
 
     }
+
+
+
+    val random = Random
+    fun rand(start: Int,finish : Int):Int{
+        return random.nextInt(finish-start)+start
+    }
+
 }
