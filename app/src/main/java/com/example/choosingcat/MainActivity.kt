21246dafc1cb50813cc9c7df0fabe59edbc852b9
Opadak <tfk1019@naver.com>
2021-02-06 01:11:16 +0900

    2021-02-06

--- app/src/main/java/com/example/choosingcat/MainActivity.kt
+++ app/src/main/java/com/example/choosingcat/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.choosingcat
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import android.view.MotionEvent
+    import kotlinx.android.synthetic.main.activity_main.*
+    import kotlinx.android.synthetic.main.random_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
 
-        first_xml_btn_start.setOnClickListener {
-            setContentView(R.layout.random_main)
+            first_xml_btn_start.setOnClickListener {
+                setContentView(R.layout.random_main)
 
 
 
+            }
         }
+    private var mActivePointerId : Int = 0
+    var i  = 0
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val pointCount = event!!.pointerCount
+        // 다중 터치 수 만큼 의 수
+        val point = Array(10,{0})
+        val pointCountx = Array(10,{0f})
+        // x좌표
+        val pointCounty = Array(10,{0f})
+        // y좌표
+
+        // 인덱스 값
+        when (event.action){
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                if (pointCount <= 9){
+                    mActivePointerId = event.getPointerId(pointCount)
+                    i = pointCount
+
+                    pointCountx[i] = event.getX(i)
+                    pointCounty[i] = event.getY(i)
+                    //터치 좌표를 저장.
+
+
+                }
+
+            }
+            else -> {}
+        }
+
+        test.setText("$pointCount")
+
+
+
+
+
+
+
+        return super.onTouchEvent(event)
+
+
     }
+
+
 }
--- app/src/main/java/com/example/choosingcat/MainRandomActivity.kt
+++ app/src/main/java/com/example/choosingcat/MainRandomActivity.kt
@@ -1,12 +1,16 @@
 package com.example.choosingcat
 
+import android.animation.ObjectAnimator
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import androidx.core.view.MotionEventCompat.getPointerId
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.random_main.*
+import kotlin.math.log
 import kotlin.random.Random
 
 class MainRandomActivity : AppCompatActivity() {
@@ -19,44 +23,10 @@ class MainRandomActivity : AppCompatActivity() {
 
 
     }
-    private var mActivePointerId : Int = 0
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
 
-        val pointCount = event!!.pointerCount
-        // 다중 터치 수 만큼 의 수
-
-
-        if (event != null) {
-            for (i in 0..pointCount) {
-                mActivePointerId = event.getPointerId(i)
-                val (x: Float, y: Float) = event.findPointerIndex(mActivePointerId).let { pointerIndex ->
-                    // Get the pointer's current position
-                    event.getX(pointerIndex) to event.getY(pointerIndex)
-
-                } // id 마다 위치를 가지고 오는거. 굳이 필요한가..?
-
-            }
-        }else false
-        // 다중 터치 id 부여
-
-           val result_random =  rand(1,pointCount)
-
-
-
-
-            test.setText("$result_random")
-
-
-        return super.onTouchEvent(event)
-
-
-    }
-
-
-
-    val random = Random
-    fun rand(start: Int,finish : Int):Int{
-        return random.nextInt(finish-start)+start
-    }
 
 }
+
+
+
+
